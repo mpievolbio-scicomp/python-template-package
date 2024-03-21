0dac3f114c84f666b59164acfa7f4d54c22b48ee
Carsten <grotec@evolbio.mpg.de>
2024-03-21 22:42:40 +0100

    Bump version: 0.1.8 → 0.1.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Python Template Package'
 copyright = '2024, Max Planck Institute for Evolutionary Biology'
 author = 'Max Planck'
-version = "0.1.8"
+version = "0.1.9"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
